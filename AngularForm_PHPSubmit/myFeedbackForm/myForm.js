335ef27fa8ca09868e1a7068b30de8478476252c
desgrafx <djones@desgrafx.com>
2014-06-25 08:05:52 -0400

    cleaning of files
    moving stuff again

--- AngularForm_PHPSubmit/myFeedbackForm/myForm.js
+++ AngularForm_PHPSubmit/myFeedbackForm/myForm.js
@@ -1,13 +1,38 @@
-function maxLength(el) {	
-					if (!('maxLength' in el)) {
-						var max = el.attributes.maxLength.value;
-						el.onkeypress = function () {
-							if (this.value.length >= max) return false;
-						};
-					}
-				}
+var formApp = angular.module('formApp', []);
 
-				maxLength(document.getElementById("summary"));
-				maxLength(document.getElementById("zip"));
-				maxLength(document.getElementById("phonenumber"));
+					
+function formController($scope, $http) {
 
+	$scope.myView = true;
+	$scope.formData = {};
+	
+	
+	$scope.processForm = function() {
+			$http({
+			        method  : 'POST',
+			        url     : 'processForm.php',
+			        data    : $.param($scope.formData),  // pass in data as strings
+			        headers : { 'Content-Type': 'application/x-www-form-urlencoded' }  // set the headers so angular passing info as form data (not request payload)
+			    })
+		        .success(function(data) {
+		            console.log(data);
+
+		            if (! data.success) {
+		            
+		        
+		                $scope.errorIssueTitle = data.errors.issueTitle;
+		                $scope.errorInputIssueDate = data.errors.inputIssueDate;
+		                $scope.errorName = data.errors.name;
+		                $scope.errorZip = data.errors.zip;
+		                $scope.errorAcctEmail = data.errors.acctEmail;
+		                $scope.errorPhonenumber = data.errors.phonenumber;
+		            } else {
+		            	
+		               	$scope.message = data.message;
+		               	$scope.myView = false;
+		            }
+		        });
+	};
+
+	
+}
