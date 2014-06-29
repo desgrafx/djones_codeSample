78e36b783de8a681e484c77c5700b54dced8ef11
desgrafx <djones@desgrafx.com>
2014-06-29 16:11:48 -0400

    NP- Starting Progress

--- AngularForm_PHPSubmit/myFeedbackForm/myForm.js
+++ AngularForm_PHPSubmit/myFeedbackForm/myForm.js
@@ -1,38 +1,40 @@
 var formApp = angular.module('formApp', []);
 
-					
+
 function formController($scope, $http) {
 
 	$scope.myView = true;
 	$scope.formData = {};
-	
-	
-	$scope.processForm = function() {
-			$http({
-			        method  : 'POST',
-			        url     : 'processForm.php',
-			        data    : $.param($scope.formData),  // pass in data as strings
-			        headers : { 'Content-Type': 'application/x-www-form-urlencoded' }  // set the headers so angular passing info as form data (not request payload)
-			    })
-		        .success(function(data) {
-		            console.log(data);
 
-		            if (! data.success) {
-		            
-		        
-		                $scope.errorIssueTitle = data.errors.issueTitle;
-		                $scope.errorInputIssueDate = data.errors.inputIssueDate;
-		                $scope.errorName = data.errors.name;
-		                $scope.errorZip = data.errors.zip;
-		                $scope.errorAcctEmail = data.errors.acctEmail;
-		                $scope.errorPhonenumber = data.errors.phonenumber;
-		            } else {
-		            	
-		               	$scope.message = data.message;
-		               	$scope.myView = false;
-		            }
-		        });
+
+	$scope.processForm = function() {
+		$http({
+			method: 'POST',
+			url: 'processForm.php',
+			data: $.param($scope.formData), // pass in data as strings
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded'
+			} // set the headers so angular passing info as form data (not request payload)
+		})
+			.success(function(data) {
+				console.log(data);
+
+				if (!data.success) {
+
+
+					$scope.errorIssueTitle = data.errors.issueTitle;
+					$scope.errorInputIssueDate = data.errors.inputIssueDate;
+					$scope.errorName = data.errors.name;
+					$scope.errorZip = data.errors.zip;
+					$scope.errorAcctEmail = data.errors.acctEmail;
+					$scope.errorPhonenumber = data.errors.phonenumber;
+				} else {
+
+					$scope.message = data.message;
+					$scope.myView = false;
+				}
+			});
 	};
 
-	
+
 }
